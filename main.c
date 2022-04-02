96873350f5214ce3cc4e39b9c40e1c7380c9647e
srsrb <robinjsoares@gmail.com>
2022-04-02 16:22:15 +0200

    jeu de tests

--- main.c
+++ main.c
@@ -7,11 +7,13 @@
 int main(){
     CellKey** liste = read_public_keys("Partie2/candidates.txt");
     HashTable* t = create_hashtable(*liste,100);
+    int c=0;
     for(int i=0;i<100;i++){
         if(t->tab[i]){
-            printf("");
+            c++;
         }
     }
+    printf("%d",c);
     delete_hashtable(t);
     delete_list_keys(liste);
     return 0;
